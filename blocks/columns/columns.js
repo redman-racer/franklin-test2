a3abb74be684f3bdab5dd551f36eb9609fce6afb
redman-racer <ababmxking@gmail.com>
2023-01-10 20:13:11 -0600

    Update columns.js

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
-  console.log(block);
   const cols = [...block.firstElementChild.children];
+  console.log(cols);
   block.classList.add(`columns-${cols.length}-cols`);
 }
